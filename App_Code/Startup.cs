35d4f773c3f5ee5265ed565de1d27b2cb8ad4aa8
GavinRoderick <GavinRoderick@computing.dundee.ac.uk>
2018-02-20 13:17:22 +0000

    New Stuff

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Agile.Startup))]
+namespace Agile
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
