0af8e47ba547714a8d3cacbbdc0d037bd0c68c9b
cameronOBrien12 <cameronobrien12@hotmail.co.uk>
2018-02-20 14:39:37 +0000

    Added Default Website

---
+++ AgileWebsite/AgileWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgileWebsite.Startup))]
+namespace AgileWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
