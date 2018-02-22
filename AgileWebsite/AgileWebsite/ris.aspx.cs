a940d33eae35a6c472fec47bf530d50bb410c3d9
Gavin Roderick <gavin.roderick26@gmail.com>
2018-02-22 02:02:39 +0000

    DB Class. Select statement works. Bootstrap updated. Basic RIS page created

---
+++ AgileWebsite/AgileWebsite/ris.aspx.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AgileWebsite
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        private MySqlDataReader reader;
+        private string query;
+        public string firstName;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DB database = new DB();
+            query = "SELECT * FROM users;";
+            reader = database.Select(query);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                firstName = (reader.GetString("first_name"));
+            }
+        }
+    }
+}
