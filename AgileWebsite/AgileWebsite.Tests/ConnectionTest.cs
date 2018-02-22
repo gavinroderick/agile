74a9e5ccbce8c2ff27d74c86083fc385b17992ab
Josh NG <joshng@hotmail.co.uk>
2018-02-22 16:47:46 +0000

    Connection Test

---
+++ AgileWebsite/AgileWebsite.Tests/ConnectionTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgileWebsite.Tests
+{
+    [TestClass]
+    public class ConnectionTest
+    {
+        [TestMethod]
+        public void ConnectionTest()
+        {
+            DBTest db = new DBTest();
+            Assert.AreEqual(true, db.OpenConnection());
+        }
+    }
+}
