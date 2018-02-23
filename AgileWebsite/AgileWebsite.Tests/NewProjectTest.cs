810a0d235fd297cfebcab69ae48376c8478264b7
Josh NG <joshng@hotmail.co.uk>
2018-02-23 14:55:06 +0000

    changes

--- AgileWebsite/AgileWebsite.Tests/NewProjectTest.cs
+++ AgileWebsite/AgileWebsite.Tests/NewProjectTest.cs
@@ -10,41 +10,13 @@ namespace AgileWebsite.Tests
         [TestMethod]
         public void TestNewProjectSuccess()
         {
-            Assert.AreEqual(true, MyMethod());
         }
 
         [TestMethod]
         public void TestNewProjectFailed()
         {
-            Assert.AreEqual(false, MyMethod());
         }
 
-        public bool MyMethod()
-        {
-            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
-            {
-                string fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
-                string SaveLocation = Server.MapPath("Projects") + "\\" + fn;
-                try
-                {
-                    File1.PostedFile.SaveAs(SaveLocation);
-                    Response.Write("The file has been uploaded.");
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    Response.Write("Error: " + ex.Message);
-                    return false;
-                    //Note: Exception.Message returns a detailed message that describes the current exception. 
-                    //For security reasons, we do not recommend that you return Exception.Message to end users in 
-                    //production environments. It would be better to put a generic error message. 
-                }
-            }
-            else
-            {
-                Response.Write("Please select a file to upload.");
-                return false;
-            }
-        }
+        
     }
 }
