1780bbebbe5d9152f4e4e28bcaffcbc938ad7027
cameronOBrien12 <cameronobrien12@hotmail.co.uk>
2018-02-23 16:26:58 +0000

    Updated edit projject page

---
+++ AgileWebsite/AgileWebsite.Tests/DownloadProject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileWebsite.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using MySql.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AgileWebsite;
+
+    namespace DownloadTests
+    {
+        [TestClass]
+        public class BankAccountTests
+        {
+            [TestMethod]
+            public void TestExistingFileDownload()
+            {
+                EditProjectPage downloadPage;
+                downloadPage = new EditProjectPage();
+                Assert.AreEqual(true, downloadPage.Download("Test31"));
+            }
+
+            [TestMethod]
+            public void TestNonExistingFileDownload()
+            {
+                EditProjectPage downloadPage;
+                downloadPage = new EditProjectPage();
+                Assert.AreEqual(true, downloadPage.Download("FalseTest"));
+            }
+        }
+    }
+}
