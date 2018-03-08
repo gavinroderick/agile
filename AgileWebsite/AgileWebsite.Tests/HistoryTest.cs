baf12c719f4163bd1d579d4f890fd1f8f54e8670
Josh NG <joshng@hotmail.co.uk>
2018-03-08 15:46:58 +0000

    Made a sedcond Test to upload with some variables so you are able to change the statment to get the correct history. Aswell as upload.

--- AgileWebsite/AgileWebsite.Tests/HistoryTest.cs
+++ AgileWebsite/AgileWebsite.Tests/HistoryTest.cs
@@ -11,8 +11,8 @@ namespace AgileWebsite.Tests
         [TestMethod]
         public void SuccessHistory()
         {
-
-            string query = "SELECT * FROM 17agileteam6db.history WHERE user = '99C008'";
+            string user = "99C008";
+            string query = "SELECT * FROM 17agileteam6db.history WHERE user = '" + user + "'";
             DBTest db = new DBTest();
             string info = "";
             MySql.Data.MySqlClient.MySqlDataReader reader = db.Select(query);
@@ -26,5 +26,19 @@ namespace AgileWebsite.Tests
             Assert.AreEqual("Upload", info);
         }
 
-    }
+
+        [TestMethod]
+        public void TestHistoryUpload()
+    {
+            DBTest db = new DBTest();
+            string user = "99C009";
+            string comment = "Uploaded the file";
+            int project_ID = 2;
+            string action = "Upload";
+            string query = "INSERT INTO 17agileteam6db.history (project_ID, user, Historycol, date_time, projectAction, Comments) VALUES (" + project_ID + ", '" + user + "', ' ', NOW(), '" + action + "', '" + comment + "')";
+
+            bool insert = db.Insert(query);
+            Assert.AreEqual(true, insert);
+        }
+}
 }
