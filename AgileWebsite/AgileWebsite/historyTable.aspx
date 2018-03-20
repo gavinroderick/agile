a4ef52e6f1a2ec4fc3cfd9dfe25b6a921c223ff1
nestor suarez <nestorsdelgado@gmail.com>
2018-03-20 02:48:34 +0000

    History page added

---
+++ AgileWebsite/AgileWebsite/historyTable.aspx
@@ -0,0 +1,35 @@
+﻿
+<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeFile="historyTable.aspx.cs" Inherits="AgileWebsite.historyTable" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <style>
+table, td, th {    
+    border: 1px solid #ddd;
+    text-align: left;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    padding: 15px;
+}
+</style>
+
+    <table >
+        <tr >
+            <td> ID </td>                        
+            <td> Project ID </td>            
+            <td>User</td> 
+            <td>Date</td>
+            <td>Action</td>
+            <td>Comments</td>
+        </tr>
+
+        <%=getWhileLoopData()%>
+
+    </table>
+</asp:Content>
---
+++ AgileWebsite/AgileWebsite/historyTable.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AgileWebsite
+{
+    public partial class historyTable : System.Web.UI.Page
+    {
+        public string getWhileLoopData()
+        {
+            DB db = new DB();
+            string htmlStr = "";
+            //db.GetConnectionStringForScott();
+            MySqlConnection thisConnection = db.GetConnectionStringForScott();
+            MySqlCommand thisCommand = thisConnection.CreateCommand();
+            thisCommand.CommandText = "SELECT * FROM history ";
+            thisConnection.Open();
+            MySqlDataReader reader = thisCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string project_ID = reader.GetString(1);
+                string user = reader.GetString(2);
+                string date_time = reader.GetString(3);
+                string projectAction = reader.GetString(4);
+                string comments = reader.GetString(5);
+
+                htmlStr += "<tr><td>" + id + "</td><td>" + project_ID + "</td><td>" + user + "</td><td>" + date_time + "</td><td>" + projectAction + "</td><td>" + comments + "</td></tr>";
+            }
+
+            thisConnection.Close();
+            return htmlStr;
+        }
+    }
+}
